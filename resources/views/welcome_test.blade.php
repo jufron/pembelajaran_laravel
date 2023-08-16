4505f830cf7773f82c999c473589a588fa0c89f8
jufron tamo ama <jufrontamoama@gmail.com>
2023-08-16 21:42:40 +0800

    belajar menambahkan sass di project laravel dan melakukan configurasi hingga berhasil

---
+++ resources/views/welcome_test.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>latihan sass</title>
+  {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
+  <link rel="stylesheet" href="{{ asset('build/assets/css/app-10065e5b.css') }}">
+</head>
+<body>
+  <div class="container">
+    <div class="card">
+      <div class="head">
+        Lorem ipsum dolor sit amet consectetur.
+      </div>
+      <div class="paragraf">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sapiente laborum fugit excepturi ad hic sit! Ratione soluta omnis vitae modi deleniti quos distinctio inventore!
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,34 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                "resources/scss/app.scss",
+                'resources/js/app.js'
+            ],
             refresh: true,
-        }),
+        })
     ],
+    build: {
+        rollupOptions: {
+        output: {
+            chunkFileNames: 'assets/js/[name]-[hash].js',
+            entryFileNames: 'assets/js/[name]-[hash].js',
+            
+            assetFileNames: ({name}) => {
+            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
+                return 'assets/images/[name]-[hash][extname]';
+            }
+            
+            if (/\.css$/.test(name ?? '')) {
+                return 'assets/css/[name]-[hash][extname]';   
+            }
+    
+            // default value
+            // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+            return 'assets/[name]-[hash][extname]';
+            },
+        },
+        }
+    },
 });
